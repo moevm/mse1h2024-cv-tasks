f98ca1765001da058e9d6c141c611bae221ac211
Aizomaki <karimmakki002@gmail.com>
2024-03-09 18:04:29 +0300

    example for F1Score

--- TestMetrics.py
+++ TestMetrics.py
@@ -1,6 +1,7 @@
 from src.metrics.AccuracyChecker import AccuracyChecker
 from src.metrics.PrecisionChecker import PrecisionChecker
 from src.metrics.RecallChecker import RecallChecker
+from src.metrics.F1ScoreChecker import F1ScoreChecker
 import numpy as np
 
 # Example
@@ -23,3 +24,8 @@ if __name__ == "__main__":
     recall = recall_checker.calculate_metric(predictions, ground_truth)
     recall_interpretation = recall_checker.interpret_result(recall)
     print(recall_interpretation)
+
+    f1_score_checker = F1ScoreChecker("F1-score")
+    f1_score = f1_score_checker.calculate_metric(predictions, ground_truth)
+    f1_score_interpretation = f1_score_checker.interpret_result(f1_score)
+    print(f1_score_interpretation)
