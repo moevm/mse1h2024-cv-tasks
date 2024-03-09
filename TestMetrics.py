62bff92f723304182113ef7a00032889ce43f49d
Aizomaki <karimmakki002@gmail.com>
2024-03-09 18:00:34 +0300

    example for recall

--- TestMetrics.py
+++ TestMetrics.py
@@ -1,5 +1,6 @@
 from src.metrics.AccuracyChecker import AccuracyChecker
 from src.metrics.PrecisionChecker import PrecisionChecker
+from src.metrics.RecallChecker import RecallChecker
 import numpy as np
 
 # Example
@@ -11,9 +12,14 @@ if __name__ == "__main__":
     precision_checker = PrecisionChecker("Precision")
     precision = precision_checker.calculate_metric(predictions, ground_truth)
     precision_interpretation = precision_checker.interpret_result(precision)
-    print("Precision:", precision_interpretation)
+    print(precision_interpretation)
 
     accuracy_checker = AccuracyChecker("Accuracy")
     accuracy = accuracy_checker.calculate_metric(predictions, ground_truth)
     accuracy_interpretation = accuracy_checker.interpret_result(accuracy)
-    print("Accuracy:", accuracy_interpretation)
+    print(accuracy_interpretation)
+
+    recall_checker = RecallChecker("Recall")
+    recall = recall_checker.calculate_metric(predictions, ground_truth)
+    recall_interpretation = recall_checker.interpret_result(recall)
+    print(recall_interpretation)
