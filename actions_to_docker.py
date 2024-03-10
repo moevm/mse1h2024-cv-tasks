c35f78b7de8523d2f57f8060c753e85faa781604
yaroslav_321 <kyaroslav03@gmail.com>
2024-03-10 23:42:19 +0300

    transit files from actions to docker

---
+++ actions_to_docker.py
@@ -0,0 +1,22 @@
+import json
+import subprocess
+import shutil
+from pathlib import Path
+
+JSON_PATH = "./right_requests.json"
+SOURCE_DIR_PATH = "."
+DEST_DIR_PATH = "./app_files"
+
+with open(JSON_PATH) as f:
+    data = json.load(f)
+print(data)
+
+for i in range (len(data)):
+    command = "gh pr checkout "+str(data[i]["number"])
+    subprocess.run(command, shell=True, executable="/bin/bash")
+    for file in data[i]["files"]:
+        source_path = file["path"]
+        destination_path = "app_files/"+source_path
+        destination_path = "/".join(destination_path.split("/")[:-1])
+        Path(destination_path).mkdir(parents=True, exist_ok=True)
+        shutil.copy(source_path, destination_path)
