c4df0109e06d170c9129df368014c7f34542cb97
wake <kakoy-to_wuk@mail.ru>
2024-04-21 15:01:12 +0300

    add sh

--- .github/actions/run-metrics/entrypoint.sh
+++ .github/actions/run-metrics/entrypoint.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-cd $GITHUB_WORKSPACE/pull-requests-data
+# Функция для обхода папок
+traverse() {
+    for file in "$1"/*; do
+      for dir in ${file}/*; do
+      # Проверяем, является ли текущая папка директорией
+        if [ -d "$dir" ]; then
+        # Заходим в папку src и скачиваем веса из файла weights_link.txt, если он там есть
+          if [ -f "$dir/src/weights_link.txt" ]; then
+            gdown --fuzzy $(cat ${file}/src/weights_link.txt) -O ${file}/src/student_file.pth
+          fi
+        fi
+      done
+    done
+}
 
-gdown --fuzzy $(cat /model_link.txt) -O /student_file.pth
-
+# Вызываем функцию для обхода и поиска файла
+traverse $GITHUB_WORKSPACE/pull-requests-data
 python main.py
