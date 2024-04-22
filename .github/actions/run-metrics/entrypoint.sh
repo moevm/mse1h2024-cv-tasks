fac0ebc4b111a1ece39771fb11def88612e6a257
wake <kakoy-to_wuk@mail.ru>
2024-04-23 00:30:54 +0300

    fix traverse

--- .github/actions/run-metrics/entrypoint.sh
+++ .github/actions/run-metrics/entrypoint.sh
@@ -2,10 +2,10 @@
 
 # Функция для обхода папок
 traverse() {
-    for dir in `find "$1"  -mindepth 2 -maxdepth 2 -type d -name src`; do
+    for dir in `find "$1"  -type d -name src`; do
         # Заходим в папку src и скачиваем веса из файла weights_link.txt, если он там есть
           if [ -f "$dir/weights_link.txt" ]; then
-            gdown --fuzzy $(cat ${dir}/src/weights_link.txt) -O ${dir}/weights.pth
+            gdown --fuzzy $(cat ${dir}/weights_link.txt) -O ${dir}/weights.pth
           fi
     done
 }
