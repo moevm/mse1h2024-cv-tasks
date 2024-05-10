b163d295189a72bea02dd5d4f9fd3051a0f29e16
Aizomaki <karimmakki002@gmail.com>
2024-05-10 21:41:17 +0300

    entrypoint.sh with onnx

--- .github/actions/run-lab2/entrypoint.sh
+++ .github/actions/run-lab2/entrypoint.sh
@@ -5,7 +5,7 @@ traverse() {
     for dir in `find "$1"  -type d -name src`; do
         # Заходим в папку src и скачиваем веса из файла weights_link.txt, если он там есть
           if [ -f "$dir/weights_link.txt" ]; then
-            gdown --fuzzy $(cat ${dir}/weights_link.txt) -O ${dir}/weights.pth
+            gdown --fuzzy $(cat ${dir}/weights_link.txt) -O ${dir}/weights.onnx
           fi
     done
 }
